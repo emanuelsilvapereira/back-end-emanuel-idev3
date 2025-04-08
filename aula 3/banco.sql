472792a2eaca578b3fe31973772c10e4268df942
emanuelsilvapereira <emanuelpereirasilvahenrique@gmail.com>
2025-04-08 09:29:16 -0300

    al

---
+++ aula 3/banco.sql
@@ -0,0 +1,35 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`usuário`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `idev3`.`usuário` (
+  `idusuário` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(200) NOT NULL,
+  `email` VARCHAR(200) NOT NULL,
+  `senha` VARCHAR(200) NOT NULL,
+  `endereço` VARCHAR(200) NOT NULL,
+  `telefone` VARCHAR(18) NOT NULL,
+  `cpf` CHAR(11) NOT NULL,
+  PRIMARY KEY (`idusuário`),
+  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
